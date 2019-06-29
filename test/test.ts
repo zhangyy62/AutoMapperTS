d802b3648065fc8330a10ee182d3b202d82f0ad1
sparkxxxxxx <5482855@qq.com>
2019-06-29 19:04:24 +0800

    add unit test

--- test/test.ts
+++ test/test.ts
@@ -5,13 +5,25 @@ import { IConfiguration, IMemberConfigurationOptions } from '../src/interfaces/i
 describe('#simpleMapping', function () {
     it('convert properties', () => {
         var objA = {prop1: 'From A', prop2: 'From A too'};
-
+        let expectedResult = {testprop1: 'From A', prop2: 'From A too'};
 
         AutoMapper.createMap('a', 'b')
         .forMember('testprop1', (opts: IMemberConfigurationOptions) => opts.mapFrom('prop1'));
         console.log('source object', objA);
         var objB = AutoMapper.map('a', 'b', objA);
         console.log('convert property prop1 to testprop1', objB);
-        assert.equal(objB, objA);
+        assert.equal(JSON.stringify(expectedResult), JSON.stringify(objB));
+    });
+
+    it('ignore properties', () => {
+        let objA = {prop1: 'From A', prop2: 'From A too'};
+        let expectedResult = {prop1: 'From A'};
+
+        AutoMapper.createMap('a', 'b')
+        .forSourceMember('prop2', (opts: IMemberConfigurationOptions) => opts.ignore());
+        console.log('source object', objA);
+        var objB = AutoMapper.map('a', 'b', objA);
+        console.log('convert property prop1 to testprop1', objB);
+        assert.equal(JSON.stringify(expectedResult), JSON.stringify(objB));
     });
 });
