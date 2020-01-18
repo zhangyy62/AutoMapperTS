ba889852ff2b6d971114f50b05e9a305a207050f
sparkxxxxxx <5482855@qq.com>
2020-01-18 17:31:20 +0800

    add tests

--- test/test.ts
+++ test/test.ts
@@ -26,4 +26,16 @@ describe('#simpleMapping', function () {
         console.log('convert property prop1 to testprop1', objB);
         assert.equal(JSON.stringify(expectedResult), JSON.stringify(objB));
     });
+
+    it('nested properties', () => {
+        var objA = { prop1: 'prop1', prop2: { prop3: 'prop3' } };
+        let expectedResult = { prop1: 'prop1', prop2: { testprop3: 'prop3' } };
+
+        AutoMapper.createMap('a', 'b')
+            .forMember('testprop3', (opts: IMemberConfigurationOptions) => opts.mapFrom('prop2.prop3'));
+        console.log('source object', objA);
+        var objB = AutoMapper.map('a', 'b', objA);
+        console.log('convert property prop2.prop3 to prop2.testprop3', objB);
+        assert.notDeepEqual(expectedResult, objB);
+    });
 });
